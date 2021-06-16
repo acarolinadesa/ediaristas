648729305341a98caaa1f3e178b58ee0702670b6
acarolren <anacarolinareisdesa@gmail.com>
2021-06-16 20:52:40 -0300

    UserInformation.style fix

--- src/ui/components/display/UserInformation/UserInformation.style.tsx
+++ src/ui/components/display/UserInformation/UserInformation.style.tsx
@@ -3,37 +3,38 @@ import { Avatar, Rating } from '@material-ui/core'
 
 
 export const UserInformationContainer = styled('div')`
-  display: grid;
-  grid-template-columns: 60px 1fr;
-  grid-template-rows: repeat(3, auto);
-  grid-template-areas:
-    'avatar username' 'avatar rating' 'avatar description';
-  background-color: ${({theme}) => theme.palette.grey[50]};
-  padding: ${({theme}) => theme.spacing(3)};
-  gap: ${({theme}) => theme.spacing(0.5) + ' ' + theme.spacing(2)};
-  align-items: center;
+    display: grid;
+    grid-template-columns: 60px 1fr;
+    grid-template-rows: repeat(3, auto);
+    grid-template-areas:
+      'avatar username' 'avatar rating' 'avatar description';
+    background-color: ${({theme}) => theme.palette.grey[50]};
+    padding: ${({theme}) => theme.spacing(3)};
+    gap: ${({theme}) => theme.spacing(0.5) + ' ' + theme.spacing(2)};
+    align-items: center;
 `
 
 export const UserName = styled('div')`
-  grid-area: username;
-  font-weight: bolder;
-  color: ${({theme}) => theme.palette.text.primary};
-  font-size: ${({theme}) => theme.typography.body2.fontsize}
+    grid-area: username;
+    font-weight: bolder;
+    color: ${({ theme }) => theme.palette.text.primary};
+    font-size: ${({ theme }) => theme.typography.body2.fontSize};
 `
+
 export const UserDescription = styled('div')`
-  grid-area: description;
-  color: ${({theme}) => theme.palette.text.secondary};
-  font-size: ${({theme}) => theme.typography.body2.fontsize}
+    grid-area: description;
+    color: ${({ theme }) => theme.palette.text.secondary};
+    font-size: ${({ theme }) => theme.typography.body2.fontSize};
 `
 export const AvatarStyled = styled(Avatar)`
-  grid-area: avatar;
-  width: 100%;
-  height: initial;
-  aspect-ratio: 1;
+    grid-area: avatar;
+    width: 100%;
+    height: initial;
+    aspect-ratio: 1;
 `
 export const RatingStyled = styled(Rating)`
-  grid-area: rating;
-  font-size: 14px;
+    grid-area: rating;
+    font-size: 14px;
 `
 
 
